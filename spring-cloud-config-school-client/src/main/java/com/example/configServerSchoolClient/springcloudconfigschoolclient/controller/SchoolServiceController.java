03bc8e983661cce9dc5e9ebb6cddad8e8e928f62
dogukan <dogukan.gungordi@gmail.com>
2019-07-19 09:33:43 +0300

    .

--- spring-cloud-config-school-client/src/main/java/com/example/configServerSchoolClient/springcloudconfigschoolclient/controller/SchoolServiceController.java
+++ spring-cloud-config-school-client/src/main/java/com/example/configServerSchoolClient/springcloudconfigschoolclient/controller/SchoolServiceController.java
@@ -2,17 +2,25 @@ package com.example.configServerSchoolClient.springcloudconfigschoolclient.contr
 
 import com.example.configServerSchoolClient.springcloudconfigschoolclient.client.StudentClient;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.*;
 
+@RefreshScope
 @RestController
 public class SchoolServiceController {
 
 
     @Autowired
     StudentClient studentClient ;
+
+    @Value("${app.service-name}")
+    private String serviceName;
+
+    @GetMapping("/service")
+    public String getServiceName() {
+        return "service name [" + this.serviceName + "]";
+    }
 
     @RequestMapping(value = "/getSchoolDetails/{schoolname}", method = RequestMethod.GET)
     public String getStudent(@PathVariable String schoolname){
